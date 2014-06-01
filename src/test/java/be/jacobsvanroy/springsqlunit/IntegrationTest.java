224e2337bf512aaef64b3894c2578a32a725dfc8
Davy Van Roy <davy.van.roy@gmail.com>
2014-06-01 21:21:38 +0200

    update license

--- src/test/java/be/jacobsvanroy/springsqlunit/IntegrationTest.java
+++ src/test/java/be/jacobsvanroy/springsqlunit/IntegrationTest.java
@@ -3,11 +3,7 @@ package be.jacobsvanroy.springsqlunit;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
@@ -25,17 +21,14 @@ import static org.fest.assertions.Assertions.assertThat;
 
 /**
  *  Copyright (C) 2014  Davy Van Roy
- *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see http://www.gnu.org/licenses/.
  */
